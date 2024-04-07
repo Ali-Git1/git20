3f92d066d0c489e15209d12b63934b001d65fd91
Ali Aliyev <elvinaliyev724@gmail.com>
2024-04-07 22:29:29 +0400

    finish

---
+++ ConsoleApp22/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public static class Extensions
+    {
+        public static int[] Add(this int[] arr, int element)
+        {
+            int[] newarr = new int[arr.Length + 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newarr[i] = arr[i];
+            }
+
+            newarr[newarr.Length - 1] = element;
+            return newarr;
+        }
+    }
+}
